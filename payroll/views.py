118fb18a53f8ff88eaa0be2fef2c489470737a3a
fatahrez <fertahrez@gmail.com>
2020-01-07 05:32:42 +0300

    in PayrollPractise/views.py add Employee class to calculate payroll for nssf, nhif and taxable_income

--- payroll/views.py
+++ payroll/views.py
@@ -1,3 +1,114 @@
 from django.shortcuts import render
 
 # Create your views here.
+class Employee:
+    basicSalary = 0
+    gross_salary = 0
+    taxable_income = 0
+    nssf_deduction = 0
+    nhif_deduction = 0
+    payee = 0
+    personal_relief = 0
+    total_tax = 0
+    net_salary =0
+
+    def __init__(self, basic_salary):
+        self.basicSalary = basic_salary
+        self.calculate_gross_salary()
+        self.calculate_nssf()
+        self.calculate_nhif()
+        self.calculate_taxable_income()
+        self.calculate_payee()
+        self.calculate_net_salary()
+
+    def calculate_gross_salary(self):
+        gross_salary = self.basicSalary
+        self.gross_salary = gross_salary
+
+        return gross_salary
+
+    def calculate_nssf(self):
+        nssf = 400
+        self.nssf_deduction = nssf
+        return nssf
+
+    def calculate_nhif(self):
+        if 0 <= self.gross_salary <= 5999:  # 0 - 5999
+            nhif = 150
+            self.nhif_deduction = nhif
+            return nhif
+        elif 6000 <= self.gross_salary <= 7999:  # 6000 - 7999
+            nhif = 300
+            self.nhif_deduction = nhif
+            return nhif
+        elif 8000 <= self.gross_salary <= 11999:
+            nhif = 400
+            self.nhif_deduction = nhif
+            return nhif
+        elif 12000 <= self.gross_salary <= 14999:  # 12000 - 14999
+            nhif = 500
+            self.nhif_deduction = nhif
+            return nhif
+        elif 15000 <= self.gross_salary < 19999:  # 15000 - 19999
+            nhif = 600
+            self.nhif_deduction = nhif
+            return nhif
+        elif 20000 <= self.gross_salary <= 24999:
+            nhif = 750
+            self.nhif_deduction = nhif
+            return nhif
+        elif 25000 <= self.gross_salary <= 29999:
+            nhif = 850
+            self.nhif_deduction = nhif
+            return nhif
+        elif 30000 <= self.gross_salary <= 34999:
+            nhif = 900
+            self.nhif_deduction = nhif
+            return nhif
+        elif 35000 <= self.gross_salary <= 39999:
+            nhif = 950
+            self.nhif_deduction = nhif
+            return nhif
+        elif 40000 <= self.gross_salary <= 44999:
+            nhif = 1000
+            self.nhif_deduction = nhif
+            return nhif
+        elif 45000 <= self.gross_salary <= 49999:
+            nhif = 1100
+            self.nhif_deduction = nhif
+            return nhif
+        elif 50000 <= self.gross_salary <= 59999:
+            nhif = 1200
+            self.nhif_deduction = nhif
+            return nhif
+        elif 60000 <= self.gross_salary <= 69999:
+            nhif = 1300
+            self.nhif_deduction = nhif
+            return nhif
+        elif 70000 <= self.gross_salary <= 79999:
+            nhif = 1400
+            self.nhif_deduction = nhif
+            return nhif
+        elif 80000 <= self.gross_salary <= 89999:
+            nhif = 1500
+            self.nhif_deduction = nhif
+            return nhif
+        elif 90000 <= self.gross_salary <= 99999:
+            nhif = 1600
+            self.nhif_deduction = nhif
+            return nhif
+        elif self.gross_salary >= 100000:
+            nhif = 1700
+            self.nhif_deduction = nhif
+            return nhif
+
+    def calculate_taxable_income(self):
+        nti = self.gross_salary - self.nssf_deduction
+        self.taxable_income = nti
+        return nti
+
+    def calculate_payee(self):
+        pass
+
+    def calculate_net_salary(self):
+        pass
