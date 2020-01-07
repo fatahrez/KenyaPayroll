8c1398a49051f6326a5ba5de6a67e34c785656f1
fatahrez <fertahrez@gmail.com>
2020-01-07 05:06:18 +0300

    create app payroll and write models EmployeeModel and PayrollModel

--- PayrollPractise/settings.py
+++ PayrollPractise/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'payroll.apps.PayrollConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,10 @@ WSGI_APPLICATION = 'PayrollPractise.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'payroll_kenya',
+        'USER': 'fatah',
+        'PASSWORD': '  ',
     }
 }
 
---
+++ payroll/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+
+# Create your models here.
+class EmployeeModel(models.Model):
+    GENDER_OPTIONS = (
+        ('M', 'Male'),
+        ('F', 'Female')
+    )
+
+    RESIDENTIAL_OPTIONS = (
+        ('R', 'Resident'),
+        ('NR', 'Non-Resident')
+    )
+
+    first_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    gender = models.CharField(max_length=1, choices=GENDER_OPTIONS)
+    date_of_birth = models.DateField()
+    residential_status = models.CharField(max_length=2, choices=RESIDENTIAL_OPTIONS)
+    national_id = models.CharField(max_length=10, unique=True)
+    kra_pin = models.CharField(max_length=11, unique=True)
+    nssf_no = models.CharField(max_length=15, unique=True)
+    nhif_no = models.CharField(max_length=15, unique=True)
+    passport_photo = models.ImageField(blank=True)
+    basic_salary = models.CharField(max_length=15)
+    bank_account_name = models.CharField(max_length=255)
+    bank_account_number = models.CharField(max_length=35)
+    bank_branch = models.CharField(max_length=40)
+    employee_job_number = models.CharField(max_length=50, null=True)
+    date_of_employment = models.DateField(null=True)
+    contract_end_date = models.DateField(null=True)
+    job_title = models.CharField(max_length=50, null=True)
+    employee_email = models.EmailField(unique=True, null=True)
+    mobile_phone = models.CharField(max_length=15, null=True)
+
+    def __str__(self):
+        return self.last_name
+
+
+class PayrollModel(models.Model):
+    month = models.CharField(max_length=30, null=True)
+    gross_pay = models.IntegerField(null=True)
+    nssf_deduction = models.IntegerField(null=True)
+    nhif_deduction = models.IntegerField(null=True)
+    payee = models.IntegerField(null=True)
+    personal_relief = models.IntegerField(null=True)
+    total_tax = models.IntegerField(null=True)
+    net_salary = models.IntegerField(null=True)
+    employee_id = models.ForeignKey(EmployeeModel, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.employee_id.last_name
