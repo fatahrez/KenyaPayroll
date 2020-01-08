9ed19e40a5cf00b9d28a02c94ee429fe9af30a11
fatahrez <fertahrez@gmail.com>
2020-01-08 23:00:08 +0300

    in settings.py add TEMPLATE_CONTEXT_PROCESSORS

--- PayrollPractise/settings.py
+++ PayrollPractise/settings.py
@@ -128,3 +128,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+    'django.core.context_processors.request',
+)
