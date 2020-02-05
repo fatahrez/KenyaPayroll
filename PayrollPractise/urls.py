e16c43fc7082717c9bd150309cd62126b827c1d3
fatahrez <fertahrez@gmail.com>
2020-02-05 18:44:04 +0300

    minor change

--- PayrollPractise/urls.py
+++ PayrollPractise/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('/', include('payroll.urls', namespace='payroll'))
+    path('', include('payroll.urls', namespace='payroll'))
 ]
